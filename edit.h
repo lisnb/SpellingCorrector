42d9dda95dd501729e634ceb3c7c542963517681
lisnb <lisnb.h@gmail.com>
2015-11-13 11:43:00 +0800

    Modify the declaration of WordEdit functions
    Returning a unordered_set instance will cause a unnecessary copy, I would use a reference as a parameter.

--- edit.h
+++ edit.h
@@ -4,8 +4,11 @@
 #include <string>
 #include <unordered_set>
 
-std::unordered_set<std::string> Word1Edit(const std::string &error);
-std::unordered_set<std::string> Word2Edit(const std::string &error);
+//std::unordered_set<std::string> Word1Edit(const std::string &error);
+//std::unordered_set<std::string> Word2Edit(const std::string &error);
+
+void Word1Edit(std::unordered_set<std::string> &uset1, const std::string &error);
+void Word2Edit(std::unordered_set<std::string> &uset2, const std::string &error);
 
 #endif // EDIT_H
 
